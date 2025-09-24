14f4e5bd00e03077938a52b7a6aab858084a13f0
JACE <yoursjace@gmail.com>
2025-09-24 10:56:52 +0530

    Delete assets directory

--- assets/js/nav.js
+++
@@ -1,4 +0,0 @@
-// Mobile menu toggle
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.getElementById('navbar');
-toggle.addEventListener('click', () => nav.classList.toggle('show'));
