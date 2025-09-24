a7e2855a7f87eadfcb9f5f9a37aa6362b11bb95a
JACE <yoursjace@gmail.com>
2025-09-24 10:55:23 +0530

    Create nav.js

---
+++ assets/js/nav.js
@@ -0,0 +1,4 @@
+// Mobile menu toggle
+const toggle = document.querySelector('.nav-toggle');
+const nav = document.getElementById('navbar');
+toggle.addEventListener('click', () => nav.classList.toggle('show'));
